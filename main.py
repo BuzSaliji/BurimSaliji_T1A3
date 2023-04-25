fea2881789b653f88874ca7ffba09723f3c48172
BuzSaliji <buz.saliji@gmail.com>
2023-04-25 20:47:44 +1000

    display_menu added

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def display_welcome_message():
     print("Welcome to the Cafe Order App!")
 
 
+def display_menu():
+    for item, price in MENU.items():
+        print(f"{item.capitalize()}: ${price:.2f}")
+
+
 def main():
     display_welcome_message()
 
