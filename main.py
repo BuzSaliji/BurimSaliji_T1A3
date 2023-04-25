65dc0932a694681f151c079a1632472d69f3a64e
BuzSaliji <buz.saliji@gmail.com>
2023-04-26 05:30:56 +1000

    total = calculate_total added to main, also print total

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ def main():
     display_welcome_message()
     display_menu()
     order = get_order()
+    total = calculate_total(order)
+    print(f"\nTotal: ${total:.2f}")
 
 
 if __name__ == "__main__":
